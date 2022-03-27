2b8d0fb06a4107b2754813357412da248abb2bd9
João Seckler <jseckler@riseup.net>
2022-03-27 19:48:17 -0300

    src: Implement attacker.c and defender.c

--- src/attacker.c
+++ src/attacker.c
@@ -9,21 +9,37 @@
 // Main header
 #include "attacker.h"
 
-// Macros
-#define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
-
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+static direction_t going = DIR_UP, last_going = DIR_STAY;
+static position_t last_position = INVALID_POSITION, opponent_position;
+
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  if (going.i != 0 && going.j == 0) {
+    printf("to aqui\n");
+    if (equal_positions(attacker_position, last_position)) {
+      opponent_position = get_spy_position(defender_spy);
+      if (last_going.j == 1) {
+        going.j = -going.j;
+      }
+      else
+        going = (direction_t)DIR_RIGHT;
+    }
+  }
+
+  else if (going.j == 1) {
+    if (equal_positions(attacker_position, last_position)) {
+      going = (direction_t)DIR_UP;
+    }
+  }
+
+  last_position = attacker_position;
+  last_going = going;
+  return going;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -9,21 +9,27 @@
 // Main header
 #include "defender.h"
 
-// Macros
-#define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
-
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+static direction_t going = DIR_UP;
+static position_t last_position = INVALID_POSITION, opponent_position;
+static int round = 0;
+
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  if (round == 5)
+    opponent_position = get_spy_position(attacker_spy);
+  round++;
+
+  if (equal_positions(last_position, defender_position)) {
+    going.i = -going.i;
+  }
+
+  last_position = defender_position;
+  return going;
 }
 
 /*----------------------------------------------------------------------------*/
